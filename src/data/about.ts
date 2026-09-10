2ddd9d8400296be3ec5393f20a377e16689ffb82
ersajanawal-lab <ersajanawal@gmail.com>
2026-09-10 20:13:03 +1000

    Revise about section with new details
    Updated personal information and travel history.

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,6 +1,6 @@
 export const aboutMarkdown = `# Intro
 
-I am the co-founder and CTO of [Promptfoo](https://promptfoo.dev), the most widely adopted open-source LLM security tool. We help teams discover and eliminate LLM risks through automated red-teaming and vulnerability analysis. Before Promptfoo, I was the VP of Engineering at [Smile ID](https://usesmileid.com), where I led teams building identity verification APIs that now process hundreds of millions of checks across Africa. I also co-founded [Arthena](https://arthena.com) (acquired by Masterworks) and [Matroid](https://matroid.com).
+I am the co-founder of [civilengineer.diary](https://www.instagram.com/civilengineer.diary), the most widely adopted open-source LLM security tool. We help teams discover and eliminate LLM risks through automated red-teaming and vulnerability analysis. Before Promptfoo, I was the VP of Engineering at [Smile ID](https://usesmileid.com), where I led teams building identity verification APIs that now process hundreds of millions of checks across Africa. I also co-founded [Arthena](https://arthena.com) (acquired by Masterworks) and [Matroid](https://matroid.com).
 
 In my spare time, I enjoy investing in people and ideas through a [small venture fund](https://skepticalinvestments.biz), focusing on projects with high social impact. If you think I can be helpful to you or your cause, or if you're interested in collaborating, feel free to get in touch.
 
@@ -39,27 +39,13 @@ Ask me in person for other stories that I'm afraid to share with the internet.
 
 # Travel / Geography
 
-- I am originally from Buffalo, New York. I have since lived in Palo Alto, Mountain View, San Francisco, Seattle, and New York City.
-- I've been to approximately 50 countries, some of which I have forgotten, many of which I would like to revisit.
-- In 2016, I visited Canada, Ethiopia, Austria, Germany, Belgium, Ireland, Northern Ireland, Italy, Romania, Sweden, Norway, Svalbard, Panama, Costa Rica, Uganda, Japan, and the UAE, mostly in that order.
-- In 2017, I visited Canada, Japan, Denmark, Germany, Sweden, Estonia, Russia, the Netherlands, Belgium, the UK, Spain, Iceland, France, Switzerland, Ethiopia, and Luxembourg.
-- In 2018, I visited Canada, France, Italy, Israel, and the UK.
-- In 2019, I visited Canada, the UK, France, and Switzerland.
-- In 2020, I traveled barely 20 blocks. I stayed in New York and remodeled an apartment.
-- In 2021, I continued remodeling an apartment.
-- In 2022, I switched jobs and visited the UK, France, Greece, Belgium, Luxembourg, Germany, and Kenya.
-- In 2023, I visited France, the UK, Ireland, and Rwanda.
-- I am an Oregon Trail II enthusiast.
+- I am originally from Bhaktapur, Nepal.
+- In 2016, I visited Gorkha, district affected by earthquake.
+- In 2025, I came to Australia for study.
 
 # Fun Facts
 
-- I have a list of thousands of ideas, like creating matching bow ties for cats and humans.
-- I almost always have a sketchbook with me and a [01 Sakura Pigma Micron Pen](https://www.sakuraofamerica.com/product/pigma-micron/).
-- I can't locate every country on a map.
-- I operate a [small angel fund](http://skepticalinvestments.biz/) with terrible returns.
-- I break about 30 traffic laws on a skateboard or [bicycle](https://www.citibikenyc.com/) every single day.
-- I stack-rank coffee shops, restaurants, and every dog I see in New York.
-- I added this page because many people complained that my site was just my resume.
+- I almost always have a sketchbook with me.
 
 # I Dream Of
 
@@ -72,22 +58,8 @@ Ask me in person for other stories that I'm afraid to share with the internet.
 
 # Websites from People I Admire
 
-- [Alex Peysakhovich](http://alexpeys.github.io/)
-- [Chris Lengerich](http://www.chrislengerich.com/)
-- [Chris Saad](https://www.chrissaad.com/)
-- [Duncan Tomlin](http://duncantomlin.com/)
-- [Ed Kearney](https://edkearney.com/)
-- [Hawley Moore](http://hawleymoore.com/)
-- [Holman Gao](https://golmansax.com/)
-- [Ian Webster](http://ianww.com/)
-- [Johanna Flato](https://www.johannaflato.com/)
-- [Judy Mou](http://www.judymou.com/)
-- [Judy Suh](https://www.judysuh.com/)
-- [Kristina Monakhova](https://kristinamonakhova.com/)
-- [Noah Trueblood](http://notrueblood.com/)
-- [Ruoxi Wang](http://ruoxiw.com/)
-- [Tom Sachs](https://www.tomsachs.org/)
-- [Will Holley](https://willholley.com/)
+- [Hany Ismael](https://planningengineer.net/about-us/)
+
 
 If we are friends and you feel like you belong on this list, you're probably right. Submit a PR, or ask me, and I'll add you.
 `;
